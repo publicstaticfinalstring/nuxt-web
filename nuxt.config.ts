cc1f40a575c4bb48c60efc4d95503d7af8d6b04e
liuyongjie <liuyj@chances.com.cn>
2024-11-15 17:50:37 +0800

    feat(网页标题): 配置全局页面的网页标题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 import Aura from '@primevue/themes/aura';
 
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: "仿写termius",
+            meta: [
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+            ],
+            charset: 'utf-8'
+        }
+    },
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     // 启用ts检查
